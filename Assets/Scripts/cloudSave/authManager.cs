eafc52d4c07ff9a560d2ebc558da7af908287c26
jiahaoguo <robinbschool@gmail.com>
2024-10-06 22:50:37 -0400

    Google Login WebGL

--- Assets/Scripts/cloudSave/authManager.cs
+++ Assets/Scripts/cloudSave/authManager.cs
@@ -5,24 +5,27 @@ using Unity.Services.Core;
 using Unity.Services.Authentication;
 using System.Threading.Tasks;
 using UnityEngine.UI;
+using Assets.SimpleSignIn.Google;
 
 public class authManager : MonoBehaviour
 {
     [HideInInspector] public Text logTxt;
-    [HideInInspector] public string facebookToken = "....";
+    [HideInInspector] static public string googleIdToken = "329035547293-h86s58ve856q1kqcjm3oi9iomrvhu02m.apps.googleusercontent.com"; // You will set the Google ID token here
     public Text playerid;
+    public Example googleExample;
 
     async void Start()
     {
         // Initialize Unity services and sign in anonymously
         await UnityServices.InitializeAsync();
-        SignIn();
+        //SignIn();
     }
 
     // This method is responsible for signing in
-    public async void SignIn()
+    public async void SignIn(string id)
     {
-        await signInAnonymous();
+        //await signInAnonymous();
+        await signInWithGoogle(id);
     }
 
     // Anonymous sign-in method
@@ -35,7 +38,6 @@ public class authManager : MonoBehaviour
             Debug.Log("Sign in Success");
             Debug.Log("Player Id:" + AuthenticationService.Instance.PlayerId);
             playerid.text = AuthenticationService.Instance.PlayerId;
-            // You can update UI elements here to show successful sign-in
         }
         catch (AuthenticationException ex)
         {
@@ -49,43 +51,28 @@ public class authManager : MonoBehaviour
         }
     }
 
-    // Facebook sign-in method (you may not need this, but it's here for reference)
-    //async Task signInWithFacebook(string token)
-    //{
-    //    try
-    //    {
-    //        await AuthenticationService.Instance.SignInWithFacebookAsync(token);
-    //        Debug.Log("Sign in with Facebook success");
-    //    }
-    //    catch (AuthenticationException ex)
-    //    {
-    //        Debug.LogError("Sign in with Facebook failed!!");
-    //        Debug.LogException(ex);
-    //    }
-    //    catch (RequestFailedException ex)
-    //    {
-    //        Debug.LogError("Request failed!!");
-    //        Debug.LogException(ex);
-    //    }
-    //}
 
-    // Steam sign-in method (you may not need this, but it's here for reference)
-    //async Task signInWithSteam(string token)
-    //{
-    //    try
-    //    {
-    //        await AuthenticationService.Instance.SignInWithSteamAsync(token);
-    //        Debug.Log("Sign in with Steam success");
-    //    }
-    //    catch (AuthenticationException ex)
-    //    {
-    //        Debug.LogError("Sign in with Steam failed!!");
-    //        Debug.LogException(ex);
-    //    }
-    //    catch (RequestFailedException ex)
-    //    {
-    //        Debug.LogError("Request failed!!");
-    //        Debug.LogException(ex);
-    //    }
-    //}
+    async Task signInWithGoogle(string googleIdToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithGoogleAsync(googleIdToken);
+
+            Debug.Log("Sign in with Google success");
+            Debug.Log("Player Id:" + AuthenticationService.Instance.PlayerId);
+            playerid.text = AuthenticationService.Instance.PlayerId;
+        }
+        catch (AuthenticationException ex)
+        {
+            Debug.LogError("Sign in with Google failed!!");
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            Debug.LogError("Request failed!!");
+            Debug.LogException(ex);
+        }
+    }
+
+    // Facebook and Steam sign-in methods (optional, from your original code)
 }
